e025953f4802b1640e55a4a3742548e31c6a1f44
Warin12 <32309540+Warin12@users.noreply.github.com>
2017-10-17 13:39:09 -0600

    bmi finished

---
+++ Bmi.android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Bmi.android
+{
+    [Activity(Label = "Bmi.android", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Xamarin.Insights.Initialize(XamarinInsights.ApiKey, this);
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+           
+        }
+    }
+}
+
